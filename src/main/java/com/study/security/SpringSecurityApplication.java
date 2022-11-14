128bc8d9b07a8cba14233622cfc212ce8977e81d
Sachin Jain <sachin.jain.nsit@gmail.com>
2022-11-14 11:24:17 +0530

    added a basic welcome controller with /welcome URI Also added spring starter security dependency.

--- src/main/java/com/study/security/SpringSecurityApplication.java
+++ src/main/java/com/study/security/SpringSecurityApplication.java
@@ -2,8 +2,10 @@ package com.study.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.study.security.controller")
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
