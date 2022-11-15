4e2b37aa9d08b6435ab690fe7b1d5e77023bfd86
Sachin Jain <sachin.jain.nsit@gmail.com>
2022-11-15 18:05:54 +0530

    Using custom Authentication Provider to make use of authentication from custom table.

---
+++ src/main/java/com/study/security/config/DemoBankAuthenticationProvider.java
@@ -0,0 +1,50 @@
+package com.study.security.config;
+
+import com.study.security.model.Customer;
+import com.study.security.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DemoBankAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    CustomerRepository customerRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String userName = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        List<Customer> customers = customerRepository.findByEmail(userName);
+        if(customers.size() == 0) {
+            throw new BadCredentialsException("No user registered with this emailId!");
+        }
+
+        Customer customer = customers.get(0);
+        if(passwordEncoder.matches(pwd, customer.getPwd())) {
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(customer.getRole()));
+            return new UsernamePasswordAuthenticationToken(userName, pwd, authorities);
+        } else {
+            throw new BadCredentialsException("Invalid credentials");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
